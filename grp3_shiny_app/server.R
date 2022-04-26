6a11af45b8d4d26bfa6d3dcfa05616965f578c4e
camfraz <cmstr.f@googlemail.com>
2022-04-26 11:29:25 +0100

    Update server.R and global.R

--- grp3_shiny_app/server.R
+++ grp3_shiny_app/server.R
@@ -12,15 +12,31 @@ library(shiny)
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
 
-    output$distPlot <- renderPlot({
-
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
-
-    })
+  joined_bed_data %>%
+    select(location_name, percentage_occupancy) %>% 
+    #You can add a filter column her by location name linked to ui, allow multiple selections.
+    group_by(location_name) %>% 
+    summarise(location_total = n(), 
+              average_bed_occupancy = round(sum(percentage_occupancy, na.rm =TRUE)/ 
+                                              location_total)) %>% 
+    mutate(quartiles = cut_number(x = average_bed_occupancy,n = 4, 
+                                  labels = c("AveOcc in Lowest 25%",
+                                             "AveOcc in Lowest 50%",
+                                             "AveOcc in Highest 50%", 
+                                             "AveOcc in Highest 75%"))
+    ) %>% 
+    ggplot()+
+    aes(x = location_name, y = average_bed_occupancy, fill = quartiles)+
+    geom_col()+
+    theme_bw() +
+    coord_flip()+
+    labs(x = "Location Name", y = "Average Bed Occupancy (%)")+
+    scale_fill_discrete(name = "Quartile")
+  
+  ggplotly(plot1)
+  
+  
+  
+  
 
 })
