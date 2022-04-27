614610848a5706cc26cf31a68922792605b3810b
Coelacanth19 <87072141+Coelacanth19@users.noreply.github.com>
2022-04-27 13:01:57 +0100

    fixing fable

--- data_prep_scripts/delayed_discharge_fable.R
+++ data_prep_scripts/delayed_discharge_fable.R
@@ -6,17 +6,12 @@ delayed_discharge_df <- read_csv(here("raw_data/delayed-discharge-beddays-health
 #POSIX compliant datetime as discrete field from original month_of_delay
 delayed_discharge_df <- delayed_discharge_df %>% 
   mutate(datetime = ym(month_of_delay), .after = month_of_delay) %>% 
-<<<<<<< Updated upstream
   filter(datetime >= "2018-04-01")
-  filter(!age == "18plus") #removing 18plus value from age column-- nothing to
-=======
-  filter(datetime >= "2018-04-01") %>% 
   filter(!age_group == "18plus") #removing 18plus value from age column-- nothing to
->>>>>>> Stashed changes
-                          #do with date
+
   
 #Writing output to a -new- csv
-  write_csv(delayed_discharge_df, "clean_data/delayed_discharge_fable.csv")
+  write_csv(delayed_discharge_df, here("clean_data/delayed_discharge_fable.csv"))
   
   
   
